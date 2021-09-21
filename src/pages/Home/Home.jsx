cf5f667c079ba218e5aa80a8606ce0aab9df4682
Ionut-Cora <cora.ionut92@yahoo.com>
2021-09-21 18:06:00 +0100

    style commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,138 +10,159 @@ import f3 from '../../utils/img/f3.jpg';
 import f9 from '../../utils/img/f9.jpg';
 import VideoFood from '../../utils/video/video-food.mp4';
 
-function Home() {
-    return(
-        <div>
-            <Layout>
-                <div id="head">
-                    <div>
+class Home extends React.Component {
+    state = {
+        isLoaded: false
+    }
 
-                        <video muted loop autoPlay id="video">
-                            <source src={VideoFood} type="video/mp4" />
-                        </video>
+    componentDidMount() {
+        this.setState({isLoaded: true});
 
-                        <h2 className="text-white">Welcome to</h2>
-                        <h1 className="text-white">Korlake Restaurant</h1>
-                        <Link to='/menu' id="menu-button">
-                            <button id="top-button">Menu</button>
-                        </Link>
-                        <Link to='/contact' id="reservation-button">
-                            <button id="top-reservation-button">Reserve table</button>
-                        </Link>
+        const video = document.querySelectorAll('video');
 
+        window.addEventListener('resize', () => {
+            if (window.matchMedia('(min-width: 1024px)').matches) {
+                console.log('mai mare de 1024');
+                video.forEach(data => data.play())
+                
+            } else {
+                console.log('mai mic de 1024');
+                video.forEach(data => data.pause())
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <Layout>
+                    <div id="head">
+                        <div>
+                            <video muted loop autoPlay id="video">
+                                <source src={VideoFood} type="video/mp4" />
+                            </video>
+    
+                            <h2 className="text-white">Welcome to</h2>
+                            <h1 className="text-white">Korlake Restaurant</h1>
+                            <Link to='/menu' id="menu-button">
+                                <button id="top-button">Menu</button>
+                            </Link>
+                            <Link to='/contact' id="reservation-button">
+                                <button id="top-reservation-button">Reserve table</button>
+                            </Link>
+                        </div>    
                     </div>
-                </div>
-
-                <div className="container d-md-flex d-sm-block" id="second-section">
-                    <div className="col-md-6 col-sm-12" id="second-img">
-                        <img src={Second} alt="second"/>
-                    </div>
-                    <div className="col-md-6 col-sm-12" id="second-section-patagraphs">
-                        <p className="m-5">
-                            Here at Korlake Restaurant we take pride in the 
-                            quality of the products used for each and every plate.
-                        </p>
-                        <p className="m-5">
-                            We will probably give excellent client benefit, 
-                            the best quality fixings, and a noteworthy affair 
-                            whether it’s an uncommon occasion, easygoing social event 
-                            of companions or a sentimental night out on the town.
-                        </p>
-
-                        <Link to="/story" id="story-button">
-                            <h3>View Our Story &#8594;</h3>
-                        </Link>
-
-                    </div>
-                </div>
-
-                <div id="menu-section">
-                    <div className="container">
-                        <h2 className="p-5">Some of Our Favorites</h2>
-
-                        <div className="d-md-flex d-sm-block justify-content-around p-5">
-                            <div className="col-md-6 col-sm-12 p-4">
-                                <h3>English Breakfast</h3>
-                                <p>
-                                    A full plate of sausages, tomatoes, mushrooms, bacon, 
-                                    black pudding, beans, served with toasted bread.
-                                </p>
-                            </div>
-
-                            <div className="col-md-6 col-sm-12 p-4">
-                                <h3>Spicy Beef Barbecue With Potatoes</h3>
-                                <p>
-                                    A juicy spicy beef stake with roast potatoes and white
-                                    creamy sauce.
-                                </p>
-                            </div>
+    
+                    <div className="container d-md-flex d-sm-block" id="second-section">
+                        <div className="col-md-6 col-sm-12" id="second-img">
+                            <img src={Second} alt="second"/>
                         </div>
-
-                        <div className="d-md-flex d-sm-block justify-content-around p-5">
-                            <div className="col-md-6 col-sm-12 p-4">
-                                <h3>Roasty Trout with vegetables</h3>
-                                <p>
-                                    A portion of roasty juicy trout, served with roast organic
-                                    vegetables from the farm.
-                                </p>
-                            </div>
-
-                            <div className="col-md-6 col-sm-12 p-4">
-                                <h3>Spaghetti Bolognese</h3>
-                                <p>
-                                    A big portion of the true italian receipe of 
-                                    spaghetti bolognese. The receipe is come directly from Italy.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="d-md-flex d-sm-block justify-content-around p-5">
-                            <div className="col-md-6 col-sm-12 p-4">
-                                <h3>Warm Chocolate Fudge Cake</h3>
-                                <p>
-                                    A creamy warm chocolate fudge cake, served with one cup of
-                                    icecream.
-                                </p>
-                            </div>
-
-                            <div className="col-md-6 col-sm-12 p-4"> 
-                                <Link to='/menu' id="menu-button">
-                                    <h3>
-                                        View Our Full Menu &#8594;
-                                    </h3>
-                                </Link>
-                            </div>
+                        <div className="col-md-6 col-sm-12" id="second-section-patagraphs">
+                            <p className="m-5">
+                                Here at Korlake Restaurant we take pride in the 
+                                quality of the products used for each and every plate.
+                            </p>
+                            <p className="m-5">
+                                We will probably give excellent client benefit, 
+                                the best quality fixings, and a noteworthy affair 
+                                whether it’s an uncommon occasion, easygoing social event 
+                                of companions or a sentimental night out on the town.
+                            </p>
+    
+                            <Link to="/story" id="story-button">
+                                <h3>View Our Story &#8594;</h3>
+                            </Link>
+    
                         </div>
                     </div>
-                </div>
-
-                <div id="food-section">
-                        <div id="food-card">
-                            <div className="food-div">
-                                <img src={f1} alt="salmon and poached egg bruschetta" />
-                                <p>Salmon and Poached Egg Bruschetta</p>
+    
+                    <div id="menu-section">
+                        <div className="container">
+                            <h2 className="p-5">Some of Our Favorites</h2>
+    
+                            <div className="d-md-flex d-sm-block justify-content-around p-5">
+                                <div className="col-md-6 col-sm-12 p-4">
+                                    <h3>English Breakfast</h3>
+                                    <p>
+                                        A full plate of sausages, tomatoes, mushrooms, bacon, 
+                                        black pudding, beans, served with toasted bread.
+                                    </p>
+                                </div>
+    
+                                <div className="col-md-6 col-sm-12 p-4">
+                                    <h3>Spicy Beef Barbecue With Potatoes</h3>
+                                    <p>
+                                        A juicy spicy beef stake with roast potatoes and white
+                                        creamy sauce.
+                                    </p>
+                                </div>
                             </div>
-                            <div className="food-div">
-                                <img src={f2} alt="pumpkin soup" />
-                                <p>Pumpkin Soup</p>
+    
+                            <div className="d-md-flex d-sm-block justify-content-around p-5">
+                                <div className="col-md-6 col-sm-12 p-4">
+                                    <h3>Roasty Trout with vegetables</h3>
+                                    <p>
+                                        A portion of roasty juicy trout, served with roast organic
+                                        vegetables from the farm.
+                                    </p>
+                                </div>
+    
+                                <div className="col-md-6 col-sm-12 p-4">
+                                    <h3>Spaghetti Bolognese</h3>
+                                    <p>
+                                        A big portion of the true italian receipe of 
+                                        spaghetti bolognese. The receipe is come directly from Italy.
+                                    </p>
+                                </div>
                             </div>
-                            <div className="food-div">
-                                <img src={f3} alt="Pork, baked potatoes, eggs and vegetables" />
-                                <p>Pork, Baked Potatoes, Eggs and Vegetables</p>
-                            </div>
-                            <div className="food-div">
-                                <img src={f9} alt="Tiramisu" />
-                                <p>Tiramisu</p>
+    
+                            <div className="d-md-flex d-sm-block justify-content-around p-5">
+                                <div className="col-md-6 col-sm-12 p-4">
+                                    <h3>Warm Chocolate Fudge Cake</h3>
+                                    <p>
+                                        A creamy warm chocolate fudge cake, served with one cup of
+                                        icecream.
+                                    </p>
+                                </div>
+    
+                                <div className="col-md-6 col-sm-12 p-4"> 
+                                    <Link to='/menu' id="menu-button">
+                                        <h3>
+                                            View Our Full Menu &#8594;
+                                        </h3>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                </div>
-
-                <Reviews />
-
-            </Layout>
-        </div>
-    )
+                    </div>
+    
+                    <div id="food-section">
+                            <div id="food-card">
+                                <div className="food-div">
+                                    <img src={f1} alt="salmon and poached egg bruschetta" />
+                                    <p>Salmon and Poached Egg Bruschetta</p>
+                                </div>
+                                <div className="food-div">
+                                    <img src={f2} alt="pumpkin soup" />
+                                    <p>Pumpkin Soup</p>
+                                </div>
+                                <div className="food-div">
+                                    <img src={f3} alt="Pork, baked potatoes, eggs and vegetables" />
+                                    <p>Pork, Baked Potatoes, Eggs and Vegetables</p>
+                                </div>
+                                <div className="food-div">
+                                    <img src={f9} alt="Tiramisu" />
+                                    <p>Tiramisu</p>
+                                </div>
+                            </div>
+                    </div>
+    
+                    <Reviews />
+    
+                </Layout>
+            </div>
+        )
+    }
 }
 
 export default Home;
