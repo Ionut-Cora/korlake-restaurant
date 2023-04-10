d9097967dde3b8c13b50e315b8c366c2953e913a
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-10 12:48:15 +0100

    font family change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
     return(
         <div className="p-3" id="footer">
-            &copy; made by Ionut Cora, 2021
+            &copy; made by Ionut Cora, 2021-2023
         </div>
     )
 }
