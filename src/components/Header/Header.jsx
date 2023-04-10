01c29f53b65fac848f9f914b753f431fd65f1b24
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-10 14:55:46 +0100

    navbar style fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import { Navbar, Nav } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
 import './Header.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
     return(
         <div id="navbar">
-            <Navbar className='bg-warning fixed-top' id="nav" expand="lg">
+            <Navbar className='fixed-top' bg="warning" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Korlake</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link className='active text-uppercase' href="/">Home</Nav.Link>
+                    <Nav.Link className='text-uppercase' href="#menu">Menu</Nav.Link>
+                    <Nav.Link className='text-uppercase' href="#story">Story</Nav.Link>
+                    <Nav.Link className='text-uppercase' href="#contact">Contact</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+
+            {/* <Navbar id="nav" expand="lg">
                 <div className="d-flex container">
-                    <Link className="text-decoration-none" id="logo" to="/">Korlake</Link>
+                    <Link id="logo" to="/">Korlake</Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
@@ -19,7 +38,7 @@ function Header() {
                         </Nav>
                     </Navbar.Collapse>
                 </div>
-            </Navbar>
+            </Navbar> */}
         </div>
     )
 }
