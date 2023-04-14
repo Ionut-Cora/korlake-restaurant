52998fb4f87b98a1fc9fef51d0f22bb2ea0b4aa6
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-14 12:26:29 +0100

    reservation modal top close button deleted

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -108,11 +108,6 @@ class Reservation extends React.Component {
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title">Thank You!</h5>
-                                <button type="button" class="btn-close btn-warning border-0 d-flex align-items-center justify-content-center" id="header-close-btn" data-bs-dismiss="modal" aria-label="Close">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                    </svg>
-                                </button>
                             </div>
                             <div class="modal-body">
                                 <p>Dear ${this.state.firstName} ${this.state.lastName}, <br>
@@ -129,12 +124,8 @@ class Reservation extends React.Component {
             `;
         }
 
-        const modalHeaderClose = document.getElementById('header-close-btn');
         const modalFooterClose = document.getElementById('footer-close-btn');
 
-        modalHeaderClose.addEventListener('click', () => {
-            window.location.reload(false);
-        });
         modalFooterClose.addEventListener('click', () => {
             window.location.reload(false);
         });
